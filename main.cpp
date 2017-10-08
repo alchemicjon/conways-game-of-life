5908da3dfa83eb6ad8359e50c9d456d4ac53edbc
thejonrichmond <jrichmond124@gmail.com>
2017-10-07 23:32:41 -0400

    initial checkin of main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+
+#include <iostream>
+using namespace std;
+
+const int NUMBER_OF_COLUMNS = 80;
+const int NUMBER_OF_ROWS = 20;
+
+void generation(char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS]);
+
+void display(const char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS]);
+
+void fillArray(char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS])
+
+int main()
+{
+  char[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] world;
+  return 0;
+}
