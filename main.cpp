de8b38c75063b15a428d4186541571dc88532940
thejonrichmond <jrichmond124@gmail.com>
2017-10-08 09:20:26 -0400

    create basic structure of program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // main.cpp
 
 #include <iostream>
+#include "game_of_life.h"
 using namespace std;
 
 const int NUMBER_OF_COLUMNS = 80;
@@ -10,10 +11,46 @@ void generation(char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS]);
 
 void display(const char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS]);
 
-void fillArray(char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS])
+void fillArray(char world[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS]);
+
+void welcome();
+
+void goodbye();
+
+void waitForInput();
 
 int main()
 {
   char[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] world;
+
+  welcome();
+  fillArray(world);
+  display(world);
+
+  for(int i = 0; i < 20; i++)
+  {
+    generation(world);
+    display(world);
+    waitForInput();
+  }
+
+  goodbye();
+
   return 0;
+}
+
+void welcome()
+{
+  cout << "Welcome to Conway's Game of Life!" << endl;
+  cout << "You may progress through each generation by pressing the Enter key" << endl;
+}
+
+void goodbye()
+{
+  cout << "Fin." << endl;
+}
+
+void waitForInput()
+{
+  cin.get();
 }
